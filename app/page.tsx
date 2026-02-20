e09b97c01dae101412327d043ef7a874e36489e8
filollan <mllanten@unimayor.edu.co>
2026-02-20 09:29:25 -0500

    api lista xd

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
-import Image from "next/image";
 
-export default function Home() {
+import Image from "next/image";
+import { getLinks } from "./lib/notion";
+
+export default async function Home() {
+const lg = await getLinks();
+
+console.log(lg);
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
